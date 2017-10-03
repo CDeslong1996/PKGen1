3739b5aeff5ddb5d5008ad0d059bb312888d6283
I864725 <chris.deslongchamp@sap.com>
2017-10-03 15:49:15 -0400

    Player class
    Player class constructor made

---
+++ player.js
@@ -0,0 +1,25 @@
+
+class Player {
+    constructor(name, gender) {
+        if(gender == 'male'){
+            this.isBoy = true;
+        }
+        else{
+            this.isBoy = false;
+        }
+        this.name = name;
+        this.badges[false,false,false,false,false,false,false,false];
+        this.money = 3000;
+        this.party[];
+        this.selectedItem;
+        this.box[];
+    }
+
+
+    
+    get PlayerName(){
+        return this.name;
+    }
+
+}
+
