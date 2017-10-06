4e8a8cf7123aad296f07056df8f839182aa19ecb
surrsurus <graymury@gmail.com>
2017-10-06 13:25:59 -0400

    Fix battle ui

--- src/spritetest.js
+++ src/spritetest.js
@@ -1,25 +1,20 @@
 var img;  // Declare variable 'img'.
-const pk = require('./src/pkjson.js');
+const pk = require('./src/pkjson');
 const Player = require('./src/player');
 
-var player = new Player('Bob', 'male');
-player.party.push(pk["CHARMANDER"]);
-player.party.push(pk["BULBASAUR"]);
+var img;
 
+let magnification = 4;
 
 function setup() {
-  createCanvas(320, 288);
-  img = loadImage("./assets/" + player.party[0].DEXNUM + ".png");  // Load the image
-  img2 = loadImage("./assets/back/" + player.party[1].DEXNUM + ".png");
+  createCanvas(164*magnification, 149*magnification);
+  img = loadImage("./assets/ui/battle.png");  // Load the image
 }
 
 function draw() {
   // Displays the image at its actual size at point (0,0)
-  image(img, 200, 0, img.width*2, img.height*2);
-  image(img2, 0, 200, img2.width*3, img2.height*3);
-  textSize(20);
-//   text("I sexually Identify as \nan \Apache Helicopter. Ever \nsince I was a boy I \ndreamed of soaring over the oilfields dropping hot sticky loads on disgusting foreigners. People say to me that a person being a helicopter is Impossible and I’m fucking retarded but I don’t care, I’m beautiful. I’m having a plastic surgeon install rotary blades, 30 mm cannons and AMG-114 Hellfire missiles on my body. From now on I want you guys to call me “Apache” and respect my right to kill from above and kill needlessly. If you can’t accept me you’re a heliphobe and need to check your vehicle privilege. Thank you for being so understanding.", 10, 30);
-//   text("this makes me moisht", 100, 200);
+  noSmooth()
+  image(img, 0, 0, img.width * magnification, img.height * magnification);
 }
 
 function run() {
