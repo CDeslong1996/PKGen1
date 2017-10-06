6045a63185fb8d0fc4fefaa50f79cf92d5f640ae
surrsurus <graymury@gmail.com>
2017-10-06 13:40:14 -0400

    Fix spritetest

--- src/spritetest.js
+++ src/spritetest.js
@@ -3,25 +3,15 @@ const pk = require('./src/pkjson');
 const Player = require('./src/player');
 const Pk = require('./src/pk');
 
-<<<<<<< HEAD
-var img;
-=======
 var player = new Player('Bob', 'male');
 player.party.push(new Pk(pk["CHARMANDER"],100,'male'));
 player.party.push(new Pk(pk["BULBASAUR"],100,'female'));
->>>>>>> 1c6ffaf3e25eaff05558dc5f3576298b3d0fe9cf
 
 let magnification = 4;
 
 function setup() {
-<<<<<<< HEAD
   createCanvas(164*magnification, 149*magnification);
   img = loadImage("./assets/ui/battle.png");  // Load the image
-=======
-  createCanvas(320, 288);
-  img = loadImage("./assets/" + player.party[0].num + ".png");  // Load the image
-  img2 = loadImage("./assets/back/" + player.party[1].num + ".png");
->>>>>>> 1c6ffaf3e25eaff05558dc5f3576298b3d0fe9cf
 }
 
 function draw() {
